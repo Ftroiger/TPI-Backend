4433ff8148059724470c3277ac0be44f0a5a67f2
Francisco <52226486+Ftroiger@users.noreply.github.com>
2023-10-28 15:50:01 -0300

    Cambios en index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+/*const express = require('express')
 
 const app = express()
 const puerto = 3001
@@ -12,5 +12,32 @@ app.get('/', (req,res) => {
 
 app.listen(puerto, () => {
     console.log(`Escuchando en el puerto ${puerto}`)
-})
-s
+})*/
+import { sequelize } from "./data/cnn_db.js"
+import  Alumno  from "./routers/alumnosR.js"
+
+const cors = require('cors');
+const express = require("express");
+
+// crear servidor
+const app = express();
+
+// Para poder leer json en el body
+app.use(express.json());
+
+app.use(cors());
+
+// Crear base si no existe
+require("./base-orm/sqlite-init");
+
+const alumnoRouter = require("./routers/alumnosR");
+app.use('/', alumnoRouter);
+
+const materiaRouter = require("./routes/Empleado");
+app.use(materiaRouter);
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor iniciado en el puerto ${port}`);
+});
