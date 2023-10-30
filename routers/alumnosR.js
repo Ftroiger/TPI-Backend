0ca7468191de3a4c4bbac96daea1bc5ef70e0ef0
MicaelaArrigoni <89210543+MicaelaArrigoni@users.noreply.github.com>
2023-10-30 16:12:48 -0300

    Add files via upload

--- routers/alumnosR.js
+++ routers/alumnosR.js
@@ -1,17 +1,23 @@
-const express = require("express");
+const express = require('express')
 const Alumnos = require('../models/alumno.js')
 
 const router = express.Router()
 
 router.get('/alumnos', async (req, res) => {
-    const alumnos = await Alumnos.findAll()
-    res.json(alumnos)
+    try {
+        const alumnos = await Alumnos.findAll()
+        res.json(alumnos)        
+    } catch(err){
+        res.status(500).json({mensaje: 'Error del servidor: ' + err.mensaje})
+    }
+
 })
 
 router.get('/alumnos/:id', async (req, res) => {
     try {
-        const id = req.params.id
-        const found = await Alumnos.findOne({ where: { id: id } })
+        const userId = req.params.id
+        const found = await Alumnos.findOne({ where: { id: userId } })
+
         if (found) {
             res.json(found)
         } else {
@@ -19,20 +25,61 @@ router.get('/alumnos/:id', async (req, res) => {
         }
         console.log(found)
     } catch (error) {
-        res.status(500).send({ mensaje: 'Error Servidor!' })
+        res.status(500).send({ mensaje: 'Error Servidor: ' + error.mensaje })
     }
 })
 
-/*router.post('/loginalumno', async(req, res) => {
-    const data = req.body
-    const user = await Alumnos.findOne({
-        where: {
+router.post('/alumnos', async (req, res) => {
+    try {
+        const data = req.body
+        const user = await Alumnos.create({
             id: data.id,
             nombre: data.nombre,
             apellido: data.apellido,
             documento: data.documento,
             fec_nacimiento: data.fec_nacimiento
+        })
+        res.sendStatus(201).json(user)
+    } catch (err){
+        res.status(400).json({mensaje: 'No se pudo agregar al alumno' + err.mensaje})
+    }
+})
+
+router.put('/alumnos/:id', async (req, res) => {
+        try{
+            const userId = req.params.id
+            const found = await Alumnos.findOne({ where: { id: userId } })
+
+            if (!found){
+                res.sendStatus(404).json({mensaje: 'Alumno no encontrado'})
+            } else {
+                const data = req.body
+                found.nombre = data.nombre
+                found.apellido = data.apellido
+                found.documento = data.documento
+                found.fec_nacimiento = data.fec_nacimiento
+
+                await found.save()
+
+                res.sendStatus(204).json({mensaje: 'Se pudo actualizar correctamente'})
+            }
+            
+
+        } catch (err){
+            res.status(500).json({ message: 'No se pudo actualizar' + err.message})
         }
-    })
-})*/
+})
+
+
+router.delete('/alumnos/:id', async (req, res) =>{
+    try {
+        const userId = req.params.id
+        await Alumnos.destroy({ where: {id: userId}})
+        res.status(204).json({mensaje: 'Alumno eliminado exitosamente'})
+    } catch(err){
+        res.status(500).json({ mensaje: 'Error del servidor: ' + err.message })
+    }
+
+})
+
 module.exports = {router}
